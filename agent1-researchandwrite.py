163f4918f1c66fcef3aec3db5e81385840640ebe
Sneha Lama <lsneha991@gmail.com>
2026-02-05 11:24:10 -0500

    add first agent research and write article, make agents and plan task

---
+++ agent1-researchandwrite.py
@@ -0,0 +1,90 @@
+# warning control
+from dbm.ndbm import library
+import warnings
+warnings.filterwarnings("ignore")
+
+#import from CrewAI library
+from crewai import Agent, Task, Crew
+
+import os 
+from utils import get_openai_api_key
+
+openai_api_key = get_openai_api_key()
+os.environ["OPENAI_MODEL_NAME"]= 'gpt-3.5-turbo'
+
+# Creating Agents
+# Define your Agents and provide role, goal and backstory
+# LLMS perform better when they are role playing
+
+# create instance of an Agent class, specially with Agent, Task and Crew
+
+# Planner Agent 
+planner = Agent (
+    role ="Content Planner",
+    goal = "Plan engaging and factually accurate content on {topic}",
+    backstory ="You are working on planning a blog article about the topic: {topic}" 
+                "You collect information that helps the audience learn something and make informed decisions"
+                "Your work is the basis for the Content Writer to write an article on this topic.",
+    allow_delegations = False,
+    verbose = True        
+)
+
+# Writer Agent
+writer = Agent(
+    role = "Content Writer",
+    goal = "Write insightful and factually accurate "
+            "Opinion piece about the topic: {topic}",
+    backstory = "You're working on a writing "
+              "a new opinion piece about the topic: {topic}. "
+              "You base your writing on the work of "
+              "the Content Planner, who provides an outline "
+              "and relevant context about the topic. "
+              "You follow the main objectives and "
+              "direction of the outline, "
+              "as provide by the Content Planner. "
+              "You also provide objective and impartial insights "
+              "and back them up with information "
+              "provide by the Content Planner. "
+              "You acknowledge in your opinion piece "
+              "when your statements are opinions "
+              "as opposed to objective statements.",
+    allow_delegation=False,
+    verbose = True
+)
+
+# Editor Agent 
+editor = Agent (
+    role = "Editor",
+    goal = "Edit a given blog post to align with "
+    "the writing style of the organization. ",
+    backstory="You are an editor who receives a blog post "
+              "from the Content Writer. "
+              "Your goal is to review the blog post "
+              "to ensure that it follows journalistic best practices,"
+              "provides balanced viewpoints "
+              "when providing opinions or assertions, "
+              "and also avoids major controversial topics "
+              "or opinions when possible.",
+    allow_delegation=False,
+    verbose=True
+)
+
+# Creating task for each agents
+
+# task for planner agent
+plan = Task (
+    description = (
+        "1. Prioritize the latest trends, key players, "
+            "and noteworthy news on {topic}.\n"
+        "2. Identify the target audience, considering "
+            "their interests and pain points.\n"
+        "3. Develop a detailed content outline including "
+            "an introduction, key points, and a call to action.\n"
+        "4. Include SEO keywords and relevant data or sources."
+    ),
+    expected_output = "A comprehensive content plan document "
+        "with an outline, audience analysis, "
+        "SEO keywords, and resources.",
+    agent = planner,
+) 
+
